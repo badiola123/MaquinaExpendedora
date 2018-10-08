b0ebee95dd430713186e89fe1240eacf409f9438
badiola123 <imanol.badiola@alumni.mondragon.edu>
2018-10-08 20:45:10 +0200

    Comments added to excepciones package for Doxygen documentation

--- src/Excepciones/FechaIncorrecta.java
+++ src/Excepciones/FechaIncorrecta.java
@@ -1,5 +1,14 @@
+/**
+ * @file FechaIncorrecta.java
+ * @author Imanol Badiola
+ * @brief This file contains an exception for the case of introducing an incorrect date
+ */
+
 package Excepciones;
 
+/**
+ * Constructor that outputs a string to console informing about the exception
+ */
 public class FechaIncorrecta extends Exception {
 	public FechaIncorrecta (){
 		super ("Introduce una fecha valida ");
--- src/Excepciones/OutOfStockException.java
+++ src/Excepciones/OutOfStockException.java
@@ -1,5 +1,14 @@
+/**
+ * @file OutOfStockException.java
+ * @author Imanol Badiola
+ * @brief This file contains an exception for the case of being a product out of stock
+ */
+
 package Excepciones;
 
+/**
+ * Constructor that outputs a string to console informing about the exception
+ */
 public class OutOfStockException extends Exception {
 	public OutOfStockException (){
 		super ("No queda stock de este producto ");
