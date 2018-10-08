8fb926d00ec599f07ec9099c62f566dbd51cdfaa
Edgar <edgar.azpiazu@alumni.mondragon.edu>
2018-10-08 22:29:44 +0200

    TipoProducto package commented

--- src/tipoProductos/ModeloColumnasTablaTipoProductos.java
+++ src/tipoProductos/ModeloColumnasTablaTipoProductos.java
@@ -1,3 +1,9 @@
+/**
+ * @file ModeloColumnasTablaTipoProducto.java
+ * @author Edgar Azpiazu
+ * @brief This file contains the table column model for product types
+ */
+
 package tipoProductos;
 
 import javax.swing.table.DefaultTableColumnModel;
@@ -7,6 +13,10 @@ public class ModeloColumnasTablaTipoProductos extends DefaultTableColumnModel {
 
 	TrazadorTablaTipoProductos trazador;
 	
+  /**
+	 * Constructor of the class, adds columns for each needed field for product type information
+	 * @param trazador Adapter for visualization style
+	 */
 	public ModeloColumnasTablaTipoProductos(TrazadorTablaTipoProductos trazador){
 		super();
 		this.trazador = trazador;
@@ -14,6 +24,13 @@ public class ModeloColumnasTablaTipoProductos extends DefaultTableColumnModel {
 		this.addColumn(crearColumna("Descripción",1,100));
 	}
 	
+  /**
+	 * Creates columns with given text, index, size and style
+	 * @param texto Text for the column
+	 * @param indice Index for column positioning
+	 * @param ancho Width of the column
+	 * @return Column created to insert into table
+	 */
 	private TableColumn crearColumna(String texto, int indice, int ancho) {
 		TableColumn columna = new TableColumn(indice,ancho);
 
