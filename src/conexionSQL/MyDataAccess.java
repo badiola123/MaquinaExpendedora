66df7a842854b89ed7b5fb7f506841fca5afec99
Edgar <edgar.azpiazu@alumni.mondragon.edu>
2018-10-19 22:55:07 +0200

    Bugs solved

--- src/conexionSQL/MyDataAccess.java
+++ src/conexionSQL/MyDataAccess.java
@@ -31,29 +31,29 @@ public class MyDataAccess {
 	 this._pwd = _pwd;
 	 
    try{
-     Class.forName("com.mysql.jdbc.Connection");
+	 Class.forName("com.mysql.jdbc.Connection");
      conn = (Connection)DriverManager.getConnection(_url, _usuario, _pwd);
-     if(conn != null) {
-       System.out.println("Conexión a base de datos "+_url+" . . . Ok");
-     }
    }
    catch(SQLException ex) {
       System.out.println("Hubo un problema al intentar conecarse a la base de datos"+_url);
    }
    catch(ClassNotFoundException ex) {
       System.out.println(ex);
-   }  
+   }finally {
+	   if(conn != null) {
+		   System.out.println("Conexión a base de datos "+_url+" . . . Ok");
+	   }
+   }
  }
+ 
  /**
    * Takes the SQL query result
    * @param _query The SQL query to be executed
    * @return ResultSet of the query 
    */
  public ResultSet getQuery(String _query) {
-    Statement state = null;
     ResultSet resultado = null;
-    try{
-      state = (Statement) conn.createStatement();
+    try(Statement state = (Statement) conn.createStatement()){
       resultado = state.executeQuery(_query);
     }
     catch(SQLException e) {
@@ -67,9 +67,9 @@ public class MyDataAccess {
    * @throws SQLException
    */
  public void setQuery(String _query) throws SQLException{
-	 Statement state = null;   
-	 state=(Statement) conn.createStatement();
-	 state.execute(_query);
+	 try(Statement state =(Statement) conn.createStatement()){
+		 state.execute(_query);
+	 }
  }
   /**
    * @return The SQL connexion
